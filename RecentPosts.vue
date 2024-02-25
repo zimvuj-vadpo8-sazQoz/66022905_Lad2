b31e38672e641b5a9f569ccc1d22992225099946
66022905 <150766273+zimvuj-vadpo8-sazQoz@users.noreply.github.com>
2024-02-25 21:41:28 +0700

    Add files via upload

---
+++ RecentPosts.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Post from './Post.vue';
+</script>
+
+<template>
+  <section class="border-2  border-sky-500 w-[292px] h-[367px]  rounded  border-dividerLight-light dark:border-dividerLight-dark cursor-pointer ">
+    <h2 class="text-[12px] font-bold leading-4 tracking-normal text-left pl-3 pb-0 pt-2 text-textPrimary-light dark:text-textPrimary-dark">RECENT POSTS</h2>
+  <a class=" grid grid-cols-1 gap-2  place-items-center  ">
+    <Post/>
+  </a>
+  </section>
+</template>
