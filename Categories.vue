f55f842ccad6f1d92331b3c8ba2753281b37c6fb
66022905 <150766273+zimvuj-vadpo8-sazQoz@users.noreply.github.com>
2024-02-25 21:40:14 +0700

    Add files via upload

---
+++ Categories.vue
@@ -0,0 +1,33 @@
+<template>
+    <section
+        class="border-[2px] border-sky-500 w-[292px] h-[199px] rounded border-dividerLight-light dark:border-dividerLight-dark cursor-pointer pl-2 py-4"
+    >
+    <h2 class="text-[12px] font-bold leading-4 tracking-normal text-left pl-1  text-textPrimary-light dark:text-textPrimary-dark">CATEGORIES</h2>
+        <section class="rounded cursor-pointer pl-0.5 border-[#3C3C3C4A] dark:border-[#545454A6]">
+            <!-- Food-->
+            <section
+                class="flex text-[16px] leading-5 w-[268px] tracking-normal text-left font-bold gap-3 pl-0.5 pb-2 pt-2 border-b border-[#3C3C3C1F] dark:border-[#5454547A] cursor-pointer text-[#3C3C3CB2] dark:text-[#EBEBEB99] hover:bg-dividerLight-light dark:hover:bg-[#4b4a4a99]"
+            >
+                <a href="/food">  Food </a>
+            </section>
+            <!-- Lifestyle-->
+            <section
+                class="flex text-[16px] leading-5 w-[268px] tracking-normal text-left font-bold gap-3 pl-0.5 pb-2 pt-2 border-b border-[#3C3C3C1F] dark:border-[#5454547A] cursor-pointer text-[#3C3C3CB2] dark:text-[#EBEBEB99] hover:bg-dividerLight-light dark:hover:bg-[#4b4a4a99]"
+            >
+                <a href="/lifestyle"> Lifestyle</a>
+            </section>
+            <!-- Studying-->
+            <section
+                class="flex text-[16px] leading-5 w-[268px] tracking-normal text-left font-bold gap-3 pl-0.5 pb-2 pt-2 border-b border-[#3C3C3C1F] dark:border-[#5454547A] text-[#3C3C3CB2] dark:text-[#EBEBEB99] hover:bg-dividerLight-light dark:hover:bg-[#4b4a4a99]"
+            >
+                <a href="/studying"> Studying </a>
+            </section>
+             <!-- Scholarship -->
+             <section
+                class="flex text-[16px] leading-5 w-[268px] tracking-normal text-left font-bold gap-3 pl-0.5 pb-2 pt-2 text-[#3C3C3CB2] dark:text-[#EBEBEB99] hover:bg-dividerLight-light dark:hover:bg-[#4b4a4a99]"
+            >
+                <a href="/scholarship "> Scholarship  </a>
+            </section>
+        </section>
+    </section>
+</template>
